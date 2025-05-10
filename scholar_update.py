5a5f2262dca79663a00fa9227cc27c21b0be0aae
raffaelemazziotti <46002983+raffaelemazziotti@users.noreply.github.com>
2025-05-10 20:40:23 +0200

    scholar update

--- scholar_update.py
+++ scholar_update.py
@@ -1,11 +1,19 @@
+from scholarly import scholarly
+import pandas as pd
+import time
 
 delay_seconds = 0.5
-search_query = scholarly.search_author('Raffaele M Mazziotti')
-print("Scholar search query object:", search_query)
-first_author_result = next(search_query)
 
-# Retrieve all the details for the author
+# search & pause
+search_query = scholarly.search_author('mazziotti raffaele')
+first_author_result = next(search_query,None)
+time.sleep(delay_seconds)
+
+# fill & pause
 author = scholarly.fill(first_author_result)
+time.sleep(delay_seconds)
+
+# assemble info
 au_info = {
     'name':        author['name'],
     'affiliation': author['affiliation'],
@@ -17,11 +25,11 @@ au_info = {
     'i10index5':   author['i10index5y'],
     'keywords':    ';'.join(author['interests']),
 }
+
+# save CSVs without index column
 pd.DataFrame(au_info, index=[0]) \
   .to_csv('data/scholar_author_info.csv', index=False)
 
 citationy = author['cites_per_year']
 pd.DataFrame(citationy, index=[0]) \
   .to_csv('data/scholar_citations_per_year.csv', index=False)
-
-print("Scholar data fetched and saved successfully.")
